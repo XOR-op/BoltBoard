f62d5ea23170b3f13c03a2370e1bae89246a4386
XOR-op <17672363+XOR-op@users.noreply.github.com>
2023-03-24 23:29:51 +0800

    setting provider

--- src/core/contexts/SettingsProvider.tsx
+++ src/core/contexts/SettingsProvider.tsx
@@ -1,96 +1,98 @@
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
-import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
+import {
+    ThemeProvider as MuiThemeProvider,
+} from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
+    createContext,
+    useContext,
+    useEffect,
+    useMemo,
+    useState,
 } from "react";
-import { useLocalStorage } from "../hooks/useLocalStorage";
-import { createTheme } from "../theme";
+import {useLocalStorage} from "../hooks/useLocalStorage";
+import {createTheme} from "../theme";
 
 interface SettingsContextInterface {
-  collapsed: boolean;
-  direction: string;
-  mode: string;
-  open: boolean;
-  changeCollapsed: (collapsed: boolean) => void;
-  changeDirection: (direction: "ltr" | "rtl") => void;
-  changeMode: (mode: string) => void;
-  toggleDrawer: () => void;
+    collapsed: boolean;
+    direction: string;
+    mode: string;
+    open: boolean;
+    changeCollapsed: (collapsed: boolean) => void;
+    changeDirection: (direction: "ltr" | "rtl") => void;
+    changeMode: (mode: string) => void;
+    toggleDrawer: () => void;
 }
 
 export const SettingsContext = createContext({} as SettingsContextInterface);
 
 type SettingsProviderProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
-const SettingsProvider = ({ children }: SettingsProviderProps) => {
-  const [collapsed, setCollapsed] = useLocalStorage("sidebarcollapsed", false);
-  const [direction, setDirection] = useLocalStorage("direction", "ltr");
-  const [mode, setMode] = useLocalStorage("mode", "light");
-  const [open, setOpen] = useState(false);
+const SettingsProvider = ({children}: SettingsProviderProps) => {
+    const [collapsed, setCollapsed] = useLocalStorage("sidebarcollapsed", false);
+    const [direction, setDirection] = useLocalStorage("direction", "ltr");
+    const [mode, setMode] = useLocalStorage("mode", "light");
+    const [open, setOpen] = useState(false);
 
-  useEffect(() => {
-    document.body.dir = direction;
-  }, [direction]);
+    useEffect(() => {
+        document.body.dir = direction;
+    }, [direction]);
 
-  const theme = useMemo(
-    () => createTheme(direction as "ltr" | "rtl", mode as "dark" | "light"),
-    [direction, mode]
-  );
+    const theme = useMemo(
+        () => createTheme(direction as "ltr" | "rtl", mode as "dark" | "light"),
+        [direction, mode]
+    );
 
-  const changeCollapsed = (collapsed: boolean) => {
-    if (typeof collapsed === "boolean") {
-      setCollapsed(collapsed);
-    }
-  };
+    const changeCollapsed = (collapsed: boolean) => {
+        if (typeof collapsed === "boolean") {
+            setCollapsed(collapsed);
+        }
+    };
 
-  const changeDirection = (direction: "ltr" | "rtl") => {
-    if (direction) {
-      setDirection(direction);
-    }
-  };
+    const changeDirection = (direction: "ltr" | "rtl") => {
+        if (direction) {
+            setDirection(direction);
+        }
+    };
 
-  const changeMode = (mode: string) => {
-    if (mode) {
-      setMode(mode);
-    }
-  };
+    const changeMode = (mode: string) => {
+        if (mode) {
+            setMode(mode);
+        }
+    };
 
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
 
-  return (
-    <SettingsContext.Provider
-      value={{
-        collapsed,
-        direction,
-        mode,
-        open,
-        changeCollapsed,
-        changeDirection,
-        changeMode,
-        toggleDrawer,
-      }}
-    >
-      <MuiThemeProvider theme={theme}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <CssBaseline />
-          {children}
-        </LocalizationProvider>
-      </MuiThemeProvider>
-    </SettingsContext.Provider>
-  );
+    return (
+        <SettingsContext.Provider
+            value={{
+                collapsed,
+                direction,
+                mode,
+                open,
+                changeCollapsed,
+                changeDirection,
+                changeMode,
+                toggleDrawer,
+            }}
+        >
+            <MuiThemeProvider theme={theme}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <CssBaseline/>
+                    {children}
+                </LocalizationProvider>
+            </MuiThemeProvider>
+        </SettingsContext.Provider>
+    );
 };
 
 export function useSettings() {
-  return useContext(SettingsContext);
+    return useContext(SettingsContext);
 }
 
 export default SettingsProvider;
