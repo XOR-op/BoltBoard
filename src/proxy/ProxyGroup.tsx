09496c477c56b214183e893ad4603ef2af9b3a80
XOR-op <17672363+XOR-op@users.noreply.github.com>
2023-03-25 16:12:12 +0800

    ProxyGroup: support collapse

--- src/proxy/ProxyGroup.tsx
+++ src/proxy/ProxyGroup.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Grid, IconButton} from "@mui/material";
+import {Collapse, Grid, IconButton} from "@mui/material";
 import ProxyWidget from "./ProxyWidget";
 import Typography from "@mui/material/Typography";
 import {makeStyles} from '@mui/styles'
@@ -63,20 +63,27 @@ const ProxyGroup = ({endpoint, data}: ProxyGroupProps) => {
 
     return (
         <React.Fragment>
-            <Typography gutterBottom component="h2" variant="h3" className={style.titleBar}>
-                {data.name + ' [ ' + currentProxy + ' ]'}
-                <IconButton onClick={handleOpen} disableFocusRipple={true} size='medium' edge={false}>
-                    {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
-                </IconButton>
-            </Typography>
+            <Grid item xs={12}>
+                <Typography gutterBottom component="h2" variant="h3" className={style.titleBar}>
+                    {data.name + ' [ ' + currentProxy + ' ]'}
+                    <IconButton onClick={handleOpen} disableFocusRipple={true} size='medium' edge={false}>
+                        {open ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>}
+                    </IconButton>
+                </Typography>
+            </Grid>
 
-            <Grid container spacing={4}>
-                {data.list.map((n, idx) =>
-                    (<Grid item xs={6} md={3} key={idx}>
-                            <ProxyWidget proxy={n} selected={n.name === currentProxy}
-                                         onClickHandler={onClickHandler}/>
-                        </Grid>
-                    ))}
+            <Grid item xs={12}>
+                <Collapse in={open}>
+                    <Grid container spacing={4}>
+                        {data.list.map((n, idx) =>
+                            (<Grid item xs={6} md={3} key={idx}>
+                                    <ProxyWidget proxy={n} selected={n.name === currentProxy}
+                                                 onClickHandler={onClickHandler}/>
+                                </Grid>
+                            ))}
+
+                    </Grid>
+                </Collapse>
             </Grid>
         </React.Fragment>
     );
