8343145a139f640d161317247225d4db34bd3cd6
XOR-op <17672363+XOR-op@users.noreply.github.com>
2023-05-06 22:48:08 +0800

    enhance: hide scrollbar and better color

--- boltboard/src/app/intercept/InterceptPage.tsx
+++ boltboard/src/app/intercept/InterceptPage.tsx
@@ -5,22 +5,7 @@ import React, {useCallback, useEffect, useState} from "react";
 import {InterceptEntryData} from "./InterceptEntry";
 import InterceptGroup from "./InterceptGroup";
 import {Box} from "@mui/material";
-import {makeStyles} from "@mui/styles";
 import {api_call} from "../../misc/request";
-
-
-const scrollbarFixStyles = makeStyles({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        overflowY: 'auto',
-        height: '100%',
-    },
-    content: {
-        flex: 1,
-    },
-});
-
 
 const InterceptPage = () => {
     const [interceptEntryData, setInterceptEntryData] = useState<Array<InterceptEntryData>>([])
--- boltboard/src/core/components/BoxedLayout.tsx
+++ boltboard/src/core/components/BoxedLayout.tsx
@@ -3,62 +3,62 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import IconButton from "@mui/material/IconButton";
-import { useTheme } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
 import Toolbar from "@mui/material/Toolbar";
 import SettingsIcon from "@mui/icons-material/Settings";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Logo from "./Logo";
 import SettingsDrawer from "./SettingsDrawer";
 
 type BoxedLayoutProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
-const BoxedLayout = ({ children }: BoxedLayoutProps) => {
-  const theme = useTheme();
-  const [settingsOpen, setSettingsOpen] = useState(false);
+const BoxedLayout = ({children}: BoxedLayoutProps) => {
+    const theme = useTheme();
+    const [settingsOpen, setSettingsOpen] = useState(false);
 
-  const handleSettingsToggle = () => {
-    setSettingsOpen(!settingsOpen);
-  };
+    const handleSettingsToggle = () => {
+        setSettingsOpen(!settingsOpen);
+    };
 
-  return (
-    <React.Fragment>
-      <GlobalStyles
-        styles={{ body: { backgroundColor: theme.palette.background.paper } }}
-      />
-      <AppBar color="transparent" position="relative" elevation={0} sx={{boxShadow: 'none'}}>
-        <Toolbar>
-          <Box sx={{ flexGrow: 1 }} />
-          <IconButton
-            aria-label="settings"
-            component="span"
-            onClick={handleSettingsToggle}
-            size="large">
-            <SettingsIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Container component="main" maxWidth="xs" sx={{ mt: 6 }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Logo sx={{ mb: 2 }} />
-          {children}
-          <Box>
-            <SettingsDrawer
-              onDrawerToggle={handleSettingsToggle}
-              open={settingsOpen}
+    return (
+        <React.Fragment>
+            <GlobalStyles
+                styles={{body: {backgroundColor: theme.palette.background.paper}}}
             />
-          </Box>
-        </Box>
-      </Container>
-    </React.Fragment>
-  );
+            <AppBar color="transparent" position="relative" elevation={0} sx={{boxShadow: 'none'}}>
+                <Toolbar>
+                    <Box sx={{flexGrow: 1}}/>
+                    <IconButton
+                        aria-label="settings"
+                        component="span"
+                        onClick={handleSettingsToggle}
+                        size="large">
+                        <SettingsIcon/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Container component="main" maxWidth="xs" sx={{mt: 6}}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Logo sx={{mb: 2}}/>
+                    {children}
+                    <Box>
+                        <SettingsDrawer
+                            onDrawerToggle={handleSettingsToggle}
+                            open={settingsOpen}
+                        />
+                    </Box>
+                </Box>
+            </Container>
+        </React.Fragment>
+    );
 };
 
 export default BoxedLayout;
