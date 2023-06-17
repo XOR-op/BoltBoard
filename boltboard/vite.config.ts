c2b795183ed988892270e43e6c1c4bbae6263b64
XOR-op <17672363+XOR-op@users.noreply.github.com>
2023-06-17 23:44:56 +0800

    vite: compile tauri target

--- boltboard/vite.config.ts
+++ boltboard/vite.config.ts
@@ -1,11 +1,24 @@
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: "./",
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-    preview: {cors: true}
+export default defineConfig(({command, mode}) => {
+    const env = loadEnv(mode, process.cwd())
+
+    if (env.VITE_MODE !== undefined && env.VITE_MODE.toLowerCase() === 'tauri') {
+        return {
+            base: "./",
+            build: {outDir: './tauri-dist'},
+            plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+            preview: {cors: true}
+        }
+    } else {
+        return {
+            base: "./",
+            plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+            preview: {cors: true}
+        }
+    }
 });
