c3464a27bea8dd2d45cd3bb6d63547951f30bd57
XOR-op <17672363+XOR-op@users.noreply.github.com>
2023-09-23 18:08:08 -0400

    feat: enhanced process filter

--- boltboard/src/app/connection/ConnectionPage.tsx
+++ boltboard/src/app/connection/ConnectionPage.tsx
@@ -40,83 +40,109 @@ const ConnectionPage = () => {
     }
     const displays = connList.map(item => data_to_display(item)).reverse();
 
-    const columns = useMemo<MRT_ColumnDef<ConnectionDisplay>[]>(() => [
-        {
-            header: 'Destination',
-            accessorKey: 'destination',
-            size: 200,
-            filterFn: 'regex',
-            enableColumnFilterModes: false
-        },
-        {
-            header: 'Proto',
-            accessorKey: 'protocol',
-            size: 70,
-            enableColumnFilterModes: false,
-            filterFn: 'equals',
-            filterSelectOptions: ['TCP', 'UDP', 'TLS', 'QUIC'],
-            filterVariant: 'select',
-        },
-        {
-            header: 'Proxy',
-            accessorKey: 'proxy',
-            size: 80,
-            enableColumnFilterModes: false
-        },
-        {
-            header: 'Inbound',
-            accessorKey: 'inbound',
-            size: 120,
-            enableColumnFilterModes: false
-        },
-        {
-            header: 'Process',
-            accessorKey: 'process',
-            size: 120,
-            columnFilterModeOptions: ['contains', 'regex'],
-            filterFn: 'contains',
-            filterVariant: 'select',
-            filterSelectOptions: displays.map(x => x.process)
-                .filter((val, idx, arr) => arr.indexOf(val) === idx).sort(),
-            renderColumnFilterModeMenuItems: ({onSelectFilterMode}) => [
-                <MenuItem key="0" onClick={() => {
-                    onSelectFilterMode('contains')
-                }}>
-                    <div>Select</div>
-                </MenuItem>,
-                <MenuItem key="1" onClick={() => {
-                    onSelectFilterMode('regex')
-                }}>
-                    <div>Regex</div>
-                </MenuItem>,
-            ],
-        },
-        {
-            header: 'Upload',
-            accessorKey: 'upload',
-            size: 100,
-            enableSorting: false,
-            enableColumnFilter: false,
-        },
-        {
-            header: 'Download',
-            accessorKey: 'download',
-            size: 100,
-            enableSorting: false,
-            enableColumnFilter: false,
-        },
-        {
-            header: 'Time',
-            accessorKey: 'time',
-            size: 200,
-            enableColumnFilter: false,
-            sortingFn: (a, b) => {
-                let aTime: number = a.getValue('start_time');
-                let bTime: number = b.getValue('start_time');
-                return aTime - bTime < 0 ? -1 : 1;
+    const [processFilterSelect, setProcessFilterSelect] = useState<boolean>(true);
+    const processList = [...displays.map(x => x.process).reduce((acc: Map<string, number>, val) => {
+        const v = acc.get(val);
+        if (v === undefined) acc.set(val, 1);
+        else acc.set(val, v + 1);
+        return acc;
+    }, new Map()).entries()]
+        .sort(([k1, v1], [k2, v2]) => {
+            const v = v2 - v1;
+            if (v === 0) return k1.localeCompare(k2);
+            else return v;
+        })
+        .map(([key, value]) => {
+            return {
+                text: `${key} (${value})`, value: key
             }
-        }
-    ], []);
+        });
+
+    const columns = useMemo<MRT_ColumnDef<ConnectionDisplay>[]>(() => [
+                {
+                    header: 'Destination',
+                    accessorKey: 'destination',
+                    size: 150,
+                    filterFn: 'regex',
+                    enableColumnFilterModes: false
+                },
+                {
+                    header: 'Proto',
+                    accessorKey: 'protocol',
+                    size: 50,
+                    enableColumnFilterModes: false,
+                    filterFn: 'equals',
+                    filterSelectOptions: ['TCP', 'UDP', 'TLS', 'QUIC'],
+                    filterVariant: 'select',
+                },
+                {
+                    header: 'Proxy',
+                    accessorKey: 'proxy',
+                    size: 80,
+                    enableColumnFilterModes: false
+                },
+                {
+                    header: 'Inbound',
+                    accessorKey: 'inbound',
+                    size: 80,
+                    enableColumnFilterModes: false
+                },
+                {
+                    header: 'Process',
+                    accessorKey: 'process',
+                    size: 80,
+                    columnFilterModeOptions: ['contains', 'regex'],
+
+                    filterFn: 'contains',
+                    filterVariant: processFilterSelect ? 'select' : 'text',
+                    filterSelectOptions: processList,
+                    renderColumnFilterModeMenuItems:
+                        ({onSelectFilterMode}) => [
+                            <MenuItem key="0" onClick={() => {
+                                setProcessFilterSelect(true)
+                                onSelectFilterMode('contains')
+                            }}>
+                                <div>Select</div>
+                            </MenuItem>,
+                            <MenuItem key="1" onClick={() => {
+                                setProcessFilterSelect(false)
+                                onSelectFilterMode('regex')
+                            }}>
+                                <div>Regex</div>
+                            </MenuItem>,
+                        ],
+                },
+                {
+                    header: 'Upload',
+                    accessorKey: 'upload',
+                    size: 50,
+                    enableSorting: false,
+                    enableColumnFilter: false,
+                    enableColumnActions: false
+                },
+                {
+                    header: 'Download',
+                    accessorKey: 'download',
+                    size: 50,
+                    enableSorting: false,
+                    enableColumnFilter: false,
+                    enableColumnActions: false
+                },
+                {
+                    header: 'Time',
+                    accessorKey: 'time',
+                    size: 100,
+                    enableColumnFilter: false,
+                    sortingFn: (a, b) => {
+                        let aTime: number = a.getValue('start_time');
+                        let bTime: number = b.getValue('start_time');
+                        return aTime - bTime < 0 ? -1 : 1;
+                    }
+                }
+            ],
+            [processFilterSelect, processList]
+        )
+    ;
     const theme = useTheme();
     return (
         <React.Fragment>
@@ -139,8 +165,8 @@ const ConnectionPage = () => {
                     sx: {}
                 }}
                 muiTableHeadCellFilterTextFieldProps={{
-                    sx: {m: '0.5rem 0 0 0rem', width: '90%'},
-                    variant: 'outlined',
+                    // sx: {m: '0.5rem 0 0 0rem', width: '90%'},
+                    // variant: 'outlined',
                 }}
 
                 // data
@@ -153,7 +179,7 @@ const ConnectionPage = () => {
                     regex: regex_filter
                 }}
                 localization={
-                    {filterRegex: 'Regex'} as any
+                    {filterContains: 'Select', filterRegex: 'Regex'} as any
                 }
 
                 // configuration
